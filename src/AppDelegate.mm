59b57ef23c50c46ee7941bfb10a778dad038025e
YellowCat98 <elcucuyisgay@gmail.com>
2024-09-08 20:35:08 +0100

    do some weird bafoonery

---
+++ src/AppDelegate.mm
@@ -0,0 +1,14 @@
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    // Set the root view controller
+    self.window.rootViewController = [[MainViewController alloc] init];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end
