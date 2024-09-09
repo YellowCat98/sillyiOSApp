270871c5ab37c9eba9aa2b8b7def3af1f42d5004
YellowCat98 <elcucuyisgay@gmail.com>
2024-09-09 14:30:20 +0100

    add a button

--- src/MainViewController.mm
+++ src/MainViewController.mm
@@ -8,7 +8,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    
+    // Set the background color
+    self.view.backgroundColor = [UIColor whiteColor];
     
     // Create a UILabel
     UILabel *helloLabel = [[UILabel alloc] initWithFrame:CGRectMake(50, 100, 200, 50)];
@@ -16,9 +18,22 @@
     helloLabel.textColor = [UIColor blackColor];
     helloLabel.textAlignment = NSTextAlignmentCenter;
     helloLabel.font = [UIFont systemFontOfSize:24];
+
+    UIButton* button = [UIButton buttonWithType:UIButtonTypeSystem];
+
+    [button setTitle@"Fart" forState:UIControlStateNormal];
+
+    button.frame = CGRectMake(100, 100, 200, 50);
+
+    [button addTarget:self action:@selector(buttonTapped) forControlEvents:UIControlEventTouchUpInside];
     
     // Add the label to the view
     [self.view addSubview:helloLabel];
+    [self.view addSubview:button];
+}
+
+- (void)buttonTapped {
+    NSLog(@"Hello World");
 }
 
 @end
